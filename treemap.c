090e0544d12df64a91837c017647606e39efc5f1
Replit user <>
2023-04-23 03:57:48 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -94,7 +94,63 @@ TreeNode * minimum(TreeNode * x)
 
 void removeNode(TreeMap * tree, TreeNode* node) 
 {
+
+
+  if (node->left == NULL && node->right == NULL)
+  {
+    if (node->parent == NULL)  tree->root = NULL; 
     
+    if(node->parent->right == node) node->parent->right = NULL;
+    else if (node->parent->left == node) node->parent->left = NULL;
+  }
+
+  if((node->left != NULL && node->right == NULL) || (node->left == NULL && node->right != NULL)) 
+  {
+
+    if(node->parent == NULL) 
+    {
+      if(node->left != NULL) tree->root = node->left; 
+      else tree->root = node->right;
+    }
+    
+    if(node->left != NULL) 
+    {
+      if(node->parent->right == node) 
+      {
+        node->parent->right = node->left;
+        node->left->parent = node->parent;
+      } 
+      else if (node->parent->left == node) 
+      {
+        node->parent->left = node->left;
+        node->left->parent = node->parent;
+      }
+      
+    } 
+    else if (node->right != NULL) 
+    {
+      if(node->parent->right == node) 
+      {
+        node->parent->right = node->right;
+        node->right->parent = node->parent;
+      } 
+      else if (node->parent->left == node) 
+      {
+        node->parent->left = node->right;
+        node->right->parent = node->parent;
+      }
+    }
+  }
+  
+  TreeNode* menorNodo = minimum(node->right);
+  
+  if(menorNodo != NULL) 
+  {
+    node->pair->key = menorNodo->pair->key;
+    node->pair->value = menorNodo->pair->value;
+  }
+ 
+  removeNode(tree, menorNodo);
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){
